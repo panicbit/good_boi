6a3cf74def1bcb515df9b39049d79030b20978fa
panicbit <panicbit.dev@gmail.com>
2019-03-20 01:15:12 +0100

    Decode extended instructions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 
 mod instruction;
-pub use instruction::Instruction;
+pub use instruction::{Instruction, ExtendedInstruction};
 
 fn main() {
     for code in 0x00..=0xFF {
         println!("0x{:02X} = {:?}", code, Instruction::decode(code));
+    }
+
+    println!();
+
+    for code in 0x00..=0xFF {
+        println!("0xCB 0x{:02X} = {:?}", code, ExtendedInstruction::decode(code));
     }
 }
