ce3b3483009bad194524f4899710ffc1973b1d5f
panicbit <panicbit.dev@gmail.com>
2019-03-23 22:17:52 +0100

    Implement tiny debugger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+use std::error::Error;
 pub use crate::core::Core;
 
 mod instruction;
@@ -5,15 +7,41 @@ mod core;
 
 fn main() {
     // let data = include_bytes!("../gb-test-roms/cpu_instrs/cpu_instrs.gb");
-    let data = include_bytes!("../gb-test-roms/cpu_instrs/individual/06-ld r,r.gb");
+    let data = include_bytes!("../gb-test-roms/cpu_instrs/individual/01-special.gb");
 
     let mut core = Core::new(data.to_vec());
 
-    core.run();
+    core.print_state();
 
-    // for (addr, code) in data.iter().cloned().enumerate() {
-    //     let instr = Instruction::decode(code);
+    loop {
+        let mut input = String::new();
 
-    //     println!("{:04X} = 0x{:02X} {:?}", addr, code, instr);
-    // }
+        print!("> ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let input = input.split_whitespace().collect::<Vec<&str>>();
+
+        let result = match &*input {
+            ["p", addr] => print_mem_u8(&core, addr),
+            [] | ["n"] => {
+                core.step();
+                core.print_state();
+                Ok(())
+            },
+            _ => Err("Unknown command".into()),
+        };
+
+        if let Err(err) = result {
+            println!("❌ {}", err);
+        }
+    }
+}
+
+fn print_mem_u8(core: &Core, addr: &str) -> Result<(), Box<Error>> {
+    let addr = u16::from_str_radix(addr, 16)?;
+    let value = core.peek_mem_u8(addr);
+
+    println!("[{:04X}] = {:02X}", addr, value);
+
+    Ok(())
 }
